b4b96e48692b93c96e438d90ce97ae896ee18324
emaadghorbani <emaadghorbani@gmail.com>
2024-06-11 10:59:34 +0330

    - Adds LocationAPI interface to be implemented by different third party APIs. - Adds api response model.

---
+++ src/LocationNinja/Features/IpLocation/Model/IpLocationApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace LocationNinja.Features.IpLocation.Model;
+
+public sealed record IpLocationApiResponse(
+    double Latitude,
+    double Longitude,
+    string Country,
+    string Region,
+    string City);
