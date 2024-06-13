85b85852cd9d0f247501e5c28e5d5305c56dc86b
emaadghorbani <emaadghorbani@gmail.com>
2024-06-13 20:17:25 +0330

    - Removes redundant private readonly fields and use primary constructor parameters directly. - Minor refactors.

--- src/LocationNinja/Features/IpLocation/IpLocationService.cs
+++ src/LocationNinja/Features/IpLocation/IpLocationService.cs
@@ -10,27 +10,18 @@ public class IpLocationService(ILocationAPI locationAPI,
                                IMapper mapper,
                                LocationNinjaDbContext locationNinjaDbContext)
 {
-    private readonly ILocationAPI _locationAPI = locationAPI;
-    private readonly IMapper _mapper = mapper;
-    private readonly LocationNinjaDbContext _locationNinjaDbContext = locationNinjaDbContext;
-
-
     public async Task<IpLocationResponse> GetLocation(string ip, CancellationToken cancellationToken = default)
     {
         Location? locationFromDB = await FetchLocationFromDB(ip, cancellationToken);
 
         if (locationFromDB is not null)
         {
-            var ipLocationResponseFromDB = _mapper.Map<IpLocationResponse>(locationFromDB);
-
-            return ipLocationResponseFromDB;
+            return mapper.Map<IpLocationResponse>(locationFromDB);
         }
 
         IpLocationApiResponse locationFromAPI = await FetchLocationFromApi(ip, cancellationToken);
 
-        var ipLocationResponseFromAPI = _mapper.Map<IpLocationResponse>(locationFromAPI);
-
-        return ipLocationResponseFromAPI;
+        return mapper.Map<IpLocationResponse>(locationFromAPI);
     }
 
     public async Task<IpLocationDetailedResponse> GetDetailedLocation(string ip, CancellationToken cancellationToken)
@@ -39,32 +30,28 @@ public class IpLocationService(ILocationAPI locationAPI,
 
         if (locationFromDB is not null)
         {
-            var ipLocationResponseFromDB = _mapper.Map<IpLocationDetailedResponse>(locationFromDB);
-
-            return ipLocationResponseFromDB;
+            return mapper.Map<IpLocationDetailedResponse>(locationFromDB);
         }
 
         IpLocationApiResponse locationFromAPI = await FetchLocationFromApi(ip, cancellationToken);
 
-        var ipLocationResponseFromAPI = _mapper.Map<IpLocationDetailedResponse>(locationFromAPI);
-
-        return ipLocationResponseFromAPI;
+        return mapper.Map<IpLocationDetailedResponse>(locationFromAPI);
     }
 
     private async Task<Location?> FetchLocationFromDB(string ip, CancellationToken cancellationToken)
     {
-        return await _locationNinjaDbContext.Locations.FirstOrDefaultAsync(l => l.Ip == ip, cancellationToken);
+        return await locationNinjaDbContext.Locations.FirstOrDefaultAsync(l => l.Ip == ip, cancellationToken);
     }
 
     private async Task<IpLocationApiResponse> FetchLocationFromApi(string ip, CancellationToken cancellationToken)
     {
-        var locationFromAPI = await _locationAPI.GetAsync(ip, cancellationToken);
+        var locationFromAPI = await locationAPI.GetAsync(ip, cancellationToken);
 
-        var locationToPersist = _mapper.Map<Location>(locationFromAPI);
+        var locationToPersist = mapper.Map<Location>(locationFromAPI);
         locationToPersist.Ip = ip;
 
-        await _locationNinjaDbContext.Locations.AddAsync(locationToPersist, cancellationToken);
-        await _locationNinjaDbContext.SaveChangesAsync(cancellationToken);
+        await locationNinjaDbContext.Locations.AddAsync(locationToPersist, cancellationToken);
+        await locationNinjaDbContext.SaveChangesAsync(cancellationToken);
 
         return locationFromAPI;
     }
