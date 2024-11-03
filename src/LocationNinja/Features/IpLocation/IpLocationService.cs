b1504c1fb5edbfb1b9fae2d3e9e1cd17e5e6358e
emaadghorbani <emaadghorbani@gmail.com>
2024-11-03 17:11:20 +0330

    Fixes dependency injection usages in IpLocationService.

--- src/LocationNinja/Features/IpLocation/IpLocationService.cs
+++ src/LocationNinja/Features/IpLocation/IpLocationService.cs
@@ -10,18 +10,22 @@ public class IpLocationService(ILocationAPI locationAPI,
                                IMapper mapper,
                                LocationNinjaDbContext locationNinjaDbContext) : IIpLocationService
 {
+    private readonly ILocationAPI _locationApi = locationAPI;
+    private readonly IMapper _mapper = mapper;
+    private readonly LocationNinjaDbContext _locationNinjaDbContext = locationNinjaDbContext;
+
     public async Task<IpLocationResponse> GetLocation(string ip, CancellationToken cancellationToken = default)
     {
         Location? locationFromDB = await FetchLocationFromDB(ip, cancellationToken);
 
         if (locationFromDB is not null)
         {
-            return mapper.Map<IpLocationResponse>(locationFromDB);
+            return _mapper.Map<IpLocationResponse>(locationFromDB);
         }
 
         IpLocationApiResponse locationFromAPI = await FetchLocationFromApi(ip, cancellationToken);
 
-        return mapper.Map<IpLocationResponse>(locationFromAPI);
+        return _mapper.Map<IpLocationResponse>(locationFromAPI);
     }
 
     public async Task<IpLocationDetailedResponse> GetDetailedLocation(string ip, CancellationToken cancellationToken = default)
@@ -30,28 +34,28 @@ public class IpLocationService(ILocationAPI locationAPI,
 
         if (locationFromDB is not null)
         {
-            return mapper.Map<IpLocationDetailedResponse>(locationFromDB);
+            return _mapper.Map<IpLocationDetailedResponse>(locationFromDB);
         }
 
         IpLocationApiResponse locationFromAPI = await FetchLocationFromApi(ip, cancellationToken);
 
-        return mapper.Map<IpLocationDetailedResponse>(locationFromAPI);
+        return _mapper.Map<IpLocationDetailedResponse>(locationFromAPI);
     }
 
     private async Task<Location?> FetchLocationFromDB(string ip, CancellationToken cancellationToken)
     {
-        return await locationNinjaDbContext.Locations.FirstOrDefaultAsync(l => l.Ip == ip, cancellationToken);
+        return await _locationNinjaDbContext.Locations.FirstOrDefaultAsync(l => l.Ip == ip, cancellationToken);
     }
 
     private async Task<IpLocationApiResponse> FetchLocationFromApi(string ip, CancellationToken cancellationToken)
     {
-        var locationFromAPI = await locationAPI.GetAsync(ip, cancellationToken);
+        var locationFromAPI = await _locationApi.GetAsync(ip, cancellationToken);
 
-        var locationToPersist = mapper.Map<Location>(locationFromAPI);
+        var locationToPersist = _mapper.Map<Location>(locationFromAPI);
         locationToPersist.Ip = ip;
 
-        await locationNinjaDbContext.Locations.AddAsync(locationToPersist, cancellationToken);
-        await locationNinjaDbContext.SaveChangesAsync(cancellationToken);
+        await _locationNinjaDbContext.Locations.AddAsync(locationToPersist, cancellationToken);
+        await _locationNinjaDbContext.SaveChangesAsync(cancellationToken);
 
         return locationFromAPI;
     }
