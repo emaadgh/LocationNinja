50989d361493646d1420f092ada90bc2b8ede5b0
emaadghorbani <emaadghorbani@gmail.com>
2024-11-04 17:12:11 +0330

    - Adds Extension class for application services' registration.
    - Cleans up Program.cs

---
+++ src/LocationNinja/Extensions/WebApplicationBuilderExtension.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using LocationNinja.Common;
+using LocationNinja.Common.Persistence;
+using LocationNinja.Features.IpLocation.Providers.IpApi;
+using LocationNinja.Features.IpLocation;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocationNinja.Extensions;
+
+public static class WebApplicationBuilderExtension
+{
+    public static void AddApplicationServices(this WebApplicationBuilder builder)
+    {
+        var domainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        builder.Services.Configure<AppSettings>(builder.Configuration);
+
+        var settings = builder.Configuration.Get<AppSettings>();
+        ArgumentNullException.ThrowIfNull(settings, nameof(settings));
+
+        builder.Services.AddDbContext<LocationNinjaDbContext>(options =>
+        {
+            options.UseMongoDB(settings.MongoDatabase.Host, settings.MongoDatabase.DatabaseName);
+        });
+
+        builder.Services.AddAutoMapper(domainAssemblies);
+
+        builder.Services.AddValidatorsFromAssemblies(domainAssemblies);
+
+        builder.Services.AddScoped<IIpLocationService, IpLocationService>();
+
+        builder.Services.AddHttpClient<ILocationAPI, IpApiProvider>(options =>
+        {
+            options.BaseAddress = new Uri(settings.Features.IpLocation.IpApiBaseUrl);
+        }).SetHandlerLifetime(Timeout.InfiniteTimeSpan);
+    }
+}
--- src/LocationNinja/Program.cs
+++ src/LocationNinja/Program.cs
@@ -1,43 +1,15 @@
-using FluentValidation;
-using LocationNinja.Common;
-using LocationNinja.Common.Persistence;
-using LocationNinja.Features.IpLocation;
+using LocationNinja.Extensions;
 using LocationNinja.Features.IpLocation.Endpoints;
-using LocationNinja.Features.IpLocation.Providers.IpApi;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var domainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IIpLocationService, IpLocationService>();
-
-builder.Services.Configure<AppSettings>(builder.Configuration);
-
-var settings = builder.Configuration.Get<AppSettings>();
-ArgumentNullException.ThrowIfNull(settings, nameof(settings));
-
-builder.Services.AddDbContext<LocationNinjaDbContext>(options =>
-{
-    options.UseMongoDB(settings.MongoDatabase.Host, settings.MongoDatabase.DatabaseName);
-});
-
-builder.Services.AddHttpClient<ILocationAPI, IpApiProvider>(options =>
-{
-    options.BaseAddress = new Uri(settings.Features.IpLocation.IpApiBaseUrl);
-}).SetHandlerLifetime(Timeout.InfiniteTimeSpan);
-
-builder.Services.AddAutoMapper(domainAssemblies);
-
-builder.Services.AddValidatorsFromAssemblies(domainAssemblies);
+builder.AddApplicationServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
